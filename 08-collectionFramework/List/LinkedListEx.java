ac420c0823d9f9f43a1b775f2681581780f96d71
Bhavesh Nafde <36959638+bhaveshnafde@users.noreply.github.com>
2020-03-23 18:31:50 +0530

    added linkedlist

---
+++ 08-collectionFramework/List/LinkedListEx.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+public class LinkedListEx{
+  public static void main(String bhavesh[]){
+    LinkedList<Integer> ll = new LinkedList<>();
+    ll.add(1);
+    ll.add(2);
+    ll.add(3);
+    System.out.println(ll);
+    System.out.println(ll.get(1));
+  }
+}
